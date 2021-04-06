1953c8a3c649c55e41accf1979fbf531d595e7f4
Semyon Stanovov <sestanovov@gmail.com>
2021-04-06 14:27:50 +0300

    9.2. Checkstyle [#510641]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, double k) {
         double h = p / (2 * (k + 1));
-        double L = h * k;
-        return L * h;
+        double l = h * k;
+        return l * h;
     }
 
     public static void main(String[] args) {
