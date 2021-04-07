4954202ac7a7f1dbb837286e3ca4ca0e56bf2139
Semyon Stanovov <sestanovov@gmail.com>
2021-04-07 12:12:40 +0300

    4.3.1. Оператор && [#510578]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,6 +1,14 @@
 package ru.job4j.condition;
 
 public class Cinema {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
     public static void access(int age) {
         System.out.println("The age of the customer is : " + age);
         if (age >= 18) {
@@ -13,5 +21,10 @@ public class Cinema {
     public static void main(String[] args) {
         Cinema.access(21);
         Cinema.access(16);
+
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
 }
