d41fbec85ff2bcdc1852712d30fef81c523bc839
Semyon Stanovov <sestanovov@gmail.com>
2021-04-07 15:40:53 +0300

    5.0.1. Сумма чисел. [#510688] Добавил свои вызовы метода с разными параметрами

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -13,5 +13,7 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+        System.out.println(sum(1, 100));
+        System.out.println(sum(20, 40));
     }
 }
