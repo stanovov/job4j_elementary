5d3124cba8ee461f48af31aa7a8232e28f8052e7
Semyon Stanovov <sestanovov@gmail.com>
2021-04-09 13:01:23 +0300

    3. Объединить два отсортированных массива

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        for (int i = 0, l = 0, r = 0; i < rsl.length; i++) {
+            if (l < left.length && r < right.length) {
+                if (left[l] < right[r]) {
+                    rsl[i] = left[l];
+                    l++;
+                } else {
+                    rsl[i] = right[r];
+                    r++;
+                }
+            } else if (l < left.length) {
+                rsl[i] = left[l];
+                l++;
+            } else {
+                rsl[i] = right[r];
+                r++;
+            }
+        }
+        return rsl;
+    }
+}
