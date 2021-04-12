a5576205c5c21310073fe5deaae9a6eb44016aac
Semyon Stanovov <sestanovov@gmail.com>
2021-04-12 11:41:02 +0300

    39. Кинотеатр

---
+++ src/ru/job4j/array/Cinema.java
@@ -0,0 +1,44 @@
+package ru.job4j.array;
+
+import java.util.Objects;
+
+public class Cinema {
+    public static Place checkEmptyPlace(Place[][] places) {
+        for (int row = 0; row < places.length; row++) {
+            for (int cell = 0; cell < places[row].length; cell++) {
+                if ((row + cell) % 2 == 0 && places[row][cell] == null) {
+                    return new Place(row, cell);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static class Place {
+        private int row;
+
+        private int cell;
+
+        public Place(int row, int cell) {
+            this.row = row;
+            this.cell = cell;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Place place = (Place) o;
+            return row == place.row && cell == place.cell;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, cell);
+        }
+    }
+}
