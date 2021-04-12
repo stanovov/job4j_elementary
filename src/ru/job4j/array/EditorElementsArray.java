8cf031eb6c2ce06fc994ccc807c8f1e6bae539ec
Semyon Stanovov <sestanovov@gmail.com>
2021-04-12 11:09:41 +0300

    34. Изменить элементы двумерного массива.

---
+++ src/ru/job4j/array/EditorElementsArray.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class EditorElementsArray {
+    public static int[][] changeData(int[][] array, int el) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = array[i][j] > el ? array[i][j] - el : 0;
+            }
+        }
+        return array;
+    }
+}
