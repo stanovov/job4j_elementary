39d1fa55bdbfe8090173fdd10a047c0469cf5f42
Semyon Stanovov <sestanovov@gmail.com>
2021-04-12 14:41:48 +0300

    57. Разница между минимумом и максимумом

---
+++ src/ru/job4j/array/SubtractMinMax.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class SubtractMinMax {
+    public static int calculate(int[] ints) {
+        int min = ints[0], max = ints[0];
+        for (int i = 1; i < ints.length; i++) {
+            if (ints[i] < min) {
+                min = ints[i];
+            }
+            if (ints[i] > max) {
+                max = ints[i];
+            }
+        }
+        return max - min;
+    }
+}
