31be3efbea00997ca0f7cbffe3e94c61e0d3a721
Semyon Stanovov <sestanovov@gmail.com>
2021-04-14 12:26:16 +0300

    3. Объединить два отсортированных массива (немного уменьшил количество строк кода)

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -6,18 +6,14 @@ public class Merge {
         for (int i = 0, l = 0, r = 0; i < rsl.length; i++) {
             if (l < left.length && r < right.length) {
                 if (left[l] < right[r]) {
-                    rsl[i] = left[l];
-                    l++;
+                    rsl[i] = left[l++];
                 } else {
-                    rsl[i] = right[r];
-                    r++;
+                    rsl[i] = right[r++];
                 }
             } else if (l < left.length) {
-                rsl[i] = left[l];
-                l++;
+                rsl[i] = left[l++];
             } else {
-                rsl[i] = right[r];
-                r++;
+                rsl[i] = right[r++];
             }
         }
         return rsl;
